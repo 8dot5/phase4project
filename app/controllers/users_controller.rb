f91e3b09ea64a90241808a7de0b1ef3176a1893c
Aaron Kaplan <aaronokaplan@gmail.com>
2022-03-25 10:36:26 -0400

    initial user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        user = User.find(session[:user_id])
+        render json: user, status: :ok
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :confirm_password)
+    end
+
+
 end
