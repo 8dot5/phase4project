457a23492ed3b6fbca0f99df0b55ec52aa1a2468
Chun Yi <8.5by11@gmail.com>
2022-03-26 12:23:33 -0400

    Removed name attribute

--- db/migrate/20220324235430_create_stars.rb
+++ db/migrate/20220324235430_create_stars.rb
@@ -5,7 +5,7 @@ class CreateStars < ActiveRecord::Migration[6.1]
       t.belongs_to :constellation, null: false, foreign_key: true
       t.string :right_ascension_hrs_mins
       t.string :declination_degs_mins
-      t.float :apparent_magnitude
+      t.float :apparent_magnitude #brighest stars have a minus apparent_magnitude 
       t.string :age
       t.float :mass_kg
       t.float :radius_km
