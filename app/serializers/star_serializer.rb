1f8fc2d445622afc8dc65349f373ba4ac2f16bab
Aaron Kaplan <aaronokaplan@gmail.com>
2022-03-28 10:08:19 -0400

    updated star model with CRUD actions + routes + serializer with distance_from_sun

--- app/serializers/star_serializer.rb
+++ app/serializers/star_serializer.rb
@@ -1,4 +1,4 @@
 class StarSerializer < ActiveModel::Serializer
-  attributes :id, :name, :right_ascension_hrs_mins, :declination_degs_mins, :apparent_magnitude, :age, :mass_kg, :radius_km, :bright_star, :image_url
+  attributes :id, :name, :right_ascension_hrs_mins, :declination_degs_mins, :apparent_magnitude, :age, :mass_kg, :radius_km, :bright_star, :distance_from_sun, :image_url
   has_one :constellation
 end
