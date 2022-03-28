2943bdfa1cf47a9a9f972096992b5550096215c2
Chun Yi <8.5by11@gmail.com>
2022-03-28 14:11:36 -0400

    Refactored Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Constellation from "./Constellation";
 
 function Home() {
 	const [constellations, setConstellations] = useState([])
@@ -7,22 +8,33 @@ function Home() {
 	useEffect(() => {
 		fetch("http://localhost:3000/constellations")
 			.then((r) => r.json())
-			.then(setConstellations);
+			.then((c) => setConstellations(c));
 	}, []);
 
+	let itemsToDisplay = constellations
+		.map(constellation => {
+			return (
+				<section className="container">
+					{constellations.map((constellation) => (
+						<div key={constellation.id} className="card">
+							<h2>
+							  <Link to={`/constellation/${constellation.id}`}>{constellation.name}</Link>
+							</h2>
+							<p>{constellation.name}</p>
+							<p>{constellation.abbreviation}</p>
+							<img alt="constellation_image" src={constellation.image_url}></img>
+						</div>
+					))}
+				</section>
+			)
+		})
+
 	return (
-		<section className="container">
-		{constellations.map((constellation) => (
-		  <div key={constellation.id} className="card">
-			<h2>
-			  <Link to={`/constellation/${constellation.id}`}>{constellation.name}</Link>
-			</h2>
-			<p>{constellation.name}</p>
-			<p>{constellation.abbreviation}</p>
-			<img alt="constellation_image" src={constellation.image_url}></img>
-		  </div>
-		))}
-	  </section>
+		<div className="cards">
+      {itemsToDisplay}
+      
+    </div>
+
 	)
 }
 
