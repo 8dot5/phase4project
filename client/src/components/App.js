ac72f542bd0c2286e961ecef4f7a1b44bc672380
Aaron Kaplan <aaronokaplan@gmail.com>
2022-03-28 15:29:55 -0400

    moved fetch from home to app component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
-import { Switch, Route } from "react-router-dom";
+import { React, useEffect, useState } from 'react';
+import { Switch, Route, useParams } from "react-router-dom";
 import Home from "./Home";
 import Constellation from "./Constellation";
 import Star from "./Star";
@@ -6,17 +7,27 @@ import Star from "./Star";
 
 
 function App() {
+	const [constellations, setConstellations] = useState([]);
+  // let { id } = useParams();
+
+
+	useEffect(() => {
+		fetch("http://localhost:3000/constellations")
+			.then((r) => r.json())
+			.then((c) => setConstellations(c));
+	}, []);
+
   return (
     <>
       <Switch>
-        <Route exact path="/constellation/:id">
-          <Constellation />
+        <Route exact path="/constellations/:id">
+          <Constellation constellations={constellations} />
         </Route>
         <Route exact path="/star/:id">
           <Star />
         </Route>
         <Route exact path="/">
-          <Home />
+          <Home constellations={constellations} />
         </Route>
       </Switch>
     </>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,21 +1,17 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Constellation from "./Constellation";
 
-function Home() {
-	const [constellations, setConstellations] = useState([])
+function Home({ constellations }) {
+	// const [constellations, setConstellations] = useState([])
 
-	useEffect(() => {
-		fetch("http://localhost:3000/constellations")
-			.then((r) => r.json())
-			.then((c) => setConstellations(c));
-	}, []);
+	// useEffect(() => {
+	// 	fetch("http://localhost:3000/constellations")
+	// 		.then((r) => r.json())
+	// 		.then((c) => setConstellations(c));
+	// }, []);
 
-	let itemsToDisplay = constellations
-		.map(constellation => {
-			return (
-				<section className="container">
-					{constellations.map((constellation) => (
+	let itemsToDisplay =
+					constellations.map((constellation) => (
 						<div key={constellation.id} className="card">
 							<h2>
 							  <Link to={`/constellation/${constellation.id}`}>{constellation.name}</Link>
@@ -24,16 +20,12 @@ function Home() {
 							<p>{constellation.abbreviation}</p>
 							<img alt="constellation_image" src={constellation.image_url}></img>
 						</div>
-					))}
-				</section>
-			)
-		})
+					))
 
 	return (
 		<div className="cards">
-      {itemsToDisplay}
-      
-    </div>
+     		 {itemsToDisplay}
+    	</div>
 
 	)
 }
