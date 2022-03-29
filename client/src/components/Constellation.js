f0d2ed34a0d2cf93828270a06d5b450d7e8c1f43
Aaron Kaplan <aaronokaplan@gmail.com>
2022-03-29 13:56:26 -0400

    fixed mapping of constellation's stars in Constellation.js, tiny modification to constellation serializer

--- client/src/components/Constellation.js
+++ client/src/components/Constellation.js
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react";
+ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 // import Star from "./Star";
 
 
 function Constellation () {
-    const [constellation, setConstellation] = useState([]);
+    const [constellation, setConstellation] = useState({});
     let { id } = useParams();
 
     useEffect(() => {
@@ -16,11 +16,15 @@ function Constellation () {
     // TODO: add edit button for authenticated users
     // TODO: add star(s) belonging to this constellation; 
     // show stars where star.constellation_id == id 
-
-    console.log(constellation.stars[0])
-
-    // let starList = constellation.stars[0].map(star => star.name)
-
+    let starList = (constellation.stars || []).map(star => {
+        return (
+        <div key={star.name}>
+            <ul>{star.name}</ul>
+            <ul>{star.id}</ul>
+        </div>
+        
+        )
+    })
     return (
         <div className="constellation-details">
 
@@ -36,7 +40,7 @@ function Constellation () {
             <ul>Number of primary stars: {constellation.main_stars}</ul>
             <ul>Origin: {constellation.origin}</ul>
             <h2>Stars:</h2>
-            {/* {constellation.stars[0]} */}
+            {starList}
         </div>
     )
 }
